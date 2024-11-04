dcff9c63aabe5ae89ab2cfa839404897deb71698
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-11-05 01:43:02 +0900

    1.2 Rust で TCP エコーサーバ

---
+++ echoserver/src/main.rs
@@ -0,0 +1,34 @@
+use core::str;
+use std::{
+    env,
+    error::Error,
+    io::{Read, Write},
+    net::TcpListener,
+    thread,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let addr = &args[1];
+    echo_server(addr)?;
+    Ok(())
+}
+
+fn echo_server(address: &str) -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind(address)?;
+    loop {
+        let (mut stream, _) = listener.accept()?;
+        // スレッドを立ち上げて接続に対処する。
+        thread::spawn(move || {
+            let mut buffer = [0u8; 1024];
+            loop {
+                let nbytes = stream.read(&mut buffer).unwrap();
+                if nbytes == 0 {
+                    return;
+                }
+                print!("{}", str::from_utf8(&buffer[..nbytes]).unwrap());
+                stream.write_all(&buffer[..nbytes]).unwrap();
+            }
+        });
+    }
+}
